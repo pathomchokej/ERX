7cbce461fc6c05766dd3ccc1c6b8768e1cf6aee0
pathomchoke <tummeng.jr@gmail.com>
2021-06-20 20:44:45 +0700

    import express framework

--- app/Server.js
+++ app/Server.js
@@ -1,1 +1,7 @@
-console.log('Test');
+const express = require('express');
+
+let server = express();
+
+server.listen(3000, function() {
+    console.log('Server Listen at http://localhost:3000');
+});
