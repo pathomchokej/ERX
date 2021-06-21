272bb894c6a12d1d543fafd31c9f45d592409abd
pathomchoke <tummeng.jr@gmail.com>
2021-06-21 13:24:29 +0700

    implement server and improve document

--- app/Server.js
+++ app/Server.js
@@ -1,8 +1,15 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const cors = require('cors');
+
 const { QustionnaireSuccess } = require('./controllers/QuestionnaireController');
 const QuestionnaireControllerModule = require('./controllers/QuestionnaireController');
 
 let _server = express();
+_server.use(bodyParser.json());
+_server.use(morgan('dev'));
+_server.use(cors());
 let _controller = QuestionnaireControllerModule.Instance;
 
 _server.listen(8000, function () {
@@ -34,7 +41,6 @@ _server.post('/createquestionnaire', function (req, res, next) {
 
 _server.get('/getquestion', function (req, res, next) {
     try {
-        console.log(req.body);
         let questionnaireIndex = Number(req.body.questionnaireIndex);
         if (questionnaireIndex < 0 || questionnaireIndex >= _controller.NumberOfQuestionnaire) {
             return res.status(404).json({
@@ -71,48 +77,62 @@ _server.get('/getquestion', function (req, res, next) {
 });
 
 _server.post('/answer', function (req, res, next) {
-    let questionnaireIndex = Number(req.body.questionnaireIndex);
-    if (questionnaireIndex < 0 || questionnaireIndex >= _controller.NumberOfQuestionnaire) {
+    try {
+        let questionnaireIndex = Number(req.body.questionnaireIndex);
+        if (questionnaireIndex < 0 || questionnaireIndex >= _controller.NumberOfQuestionnaire) {
+            return res.status(404).json({
+                code: 1,
+                message: 'Questionnaire index is out of range',
+            })
+        }
+
+        let questionIndex = Number(req.body.questionIndex);
+        let answer = req.body.ans;
+        let result = _controller.SetAnswer(questionnaireIndex, questionIndex, answer);
+        if (result.Result == QustionnaireSuccess) {
+            return res.status(200).json({
+                code: 1,
+                message: 'OK',
+            })
+        }
+        else {
+            return res.status(204).json({
+                code: 1,
+                message: 'Question index is out of range',
+            })
+
+        }
+    }
+    catch (error) {
         return res.status(404).json({
             code: 1,
-            message: 'Questionnaire index is out of range',
+            message: error,
         })
-    }
-
-    let questionIndex = Number(req.body.questionIndex);
-    let answer = req.body.answer;
-    let result = _controller.SetAnswer(questionnaireIndex, questionIndex, answer);
-    if (result.Result == QustionnaireSuccess) {
-        let question = { name: result.Data.Name, choices: result.Data.Choices };
-        return res.status(201).json({
-            code: 1,
-            message: 'OK',
-            data: question
-        })
-    }
-    else {
-        return res.status(204).json({
-            code: 1,
-            message: 'Question index is out of range',
-        })
-
     }
 });
 
 _server.get('/csv', function (req, res, next) {
-    let questionnaireIndex = Number(req.body.questionnaireIndex);
-    let result = _controller.GenerateCSV(questionnaireIndex);
-    if (result.Result == QustionnaireSuccess) {
-        return res.status(200).json({
-            code: 1,
-            message: 'OK',
-            data: result.Data
-        })
+    try {
+        let questionnaireIndex = Number(req.body.questionnaireIndex);
+        let result = _controller.GenerateCSV(questionnaireIndex);
+        if (result.Result == QustionnaireSuccess) {
+            return res.status(200).json({
+                code: 1,
+                message: 'OK',
+                data: result.Data
+            })
+        }
+        else {
+            return res.status(404).json({
+                code: 1,
+                message: 'Questionnaire index is out of range',
+            })
+        }
     }
-    else {
+    catch (error) {
         return res.status(404).json({
             code: 1,
-            message: 'Questionnaire index is out of range',
+            message: error,
         })
     }
 });
